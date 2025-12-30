f7c49d1a6d128aa0e6bf01d0570aaa889a2d4c36
Mohit Mittsl <mittalmohit2003@gmail.com>
2025-12-31 01:47:57 +0530

    fix: update CORS to allow requests from client domain

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+import express from "express";
+import cors from "cors";
 require("dotenv").config();
 
-app.use(cors());
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "https://purple-merit-yb3c.vercel.app",
+    "http://localhost:3000",
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
